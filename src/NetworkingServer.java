366f55690b614b4ab39e3388a911902af0000a8a
nazaninh0 <nazanin.heydari@learnet.se>
2024-05-28 18:40:15 +0200

    add some code

--- src/NetworkingServer.java
+++ src/NetworkingServer.java
@@ -5,7 +5,7 @@ public class NetworkingServer {
         ServerSocket server = null;
         Socket client;
         int portnumber = 1234;
-        if (args.length >= 1){
+        if (args.length >= 1) {
             portnumber = Integer.parseInt(args[0]);
         }
 
@@ -16,5 +16,35 @@ public class NetworkingServer {
             System.out.println("Cannot open socket." + ie);
             System.exit(1);
         }
+        System.out.println("ServerSocket is Created" + server);
+        while (true) {
+            try {
+                System.out.println("Waiting for connect request...");
+                client = server.accept();
+                System.out.println("");
+                String clientHost = client.getInetAddress().getHostAddress();
+                int clientPort = client.getPort();
+                System.out.println("Client host = " + clientHost + " Client port = " + clientPort);
+                InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+                String msgFromClient = br.readLine();
+                System.out.println("Message received from client = " + msgFromClient);
+                if (msgFromClient != null && !msgFromClient.equalsIgnoreCase("bye")) {
+                    OutputStream clientOut = client.getOutputStream();
+                    PrintWriter pw = new PrintWriter(clientOut, true);
+                    String ansMsg = "Hello" + msgFromClient;
+                    pw.println(ansMsg);
+                    if (msgFromClient != null && msgFromClient.equalsIgnoreCase("bye")) {
+                        server.close();
+                        client.close();
+                        break;
+                    }
+                }
+
+            } catch (IOException ie) {
+
+            }
+        }
     }
 }
+
